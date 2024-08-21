5ce98d05e984c53598065c9254a86265e3aa345b
Viktor Riabkov <vriabkov@scnsoft.com>
2024-08-21 10:56:01 +0200

    Create migrations folder for sequelize

---
+++ migrations/20240820214537-create-users-table.js
@@ -0,0 +1,56 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      telegramId: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        unique: true,
+      },
+      languageCode: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      isBot: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  },
+};
